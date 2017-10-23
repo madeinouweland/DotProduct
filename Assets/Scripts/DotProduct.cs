3c11c6fbd12d03efdafb48eebda40610f2fbd40d
hauptstadtcoder <hauptstadtcoder@fastmail.com>
2017-10-23 15:21:09 +0200

    make it so!

---
+++ Assets/Scripts/DotProduct.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DotProduct : MonoBehaviour
+{
+    public Text Text;
+    public GameObject Hero;
+    public GameObject Villain;
+
+    private void Update()
+    {
+        CalculateDotProduct();
+    }
+
+    private void CalculateDotProduct()
+    {
+        var text = "";
+
+        var heroForward = Hero.transform.forward;
+        var youToVillain = Villain.transform.position - Hero.transform.position;
+
+        text += "\nForward x[" + heroForward.x + "] z[" + heroForward.z + "]";
+        text += "\nYou to villain x[" + youToVillain.x + "] z[" + youToVillain.z + "]";
+
+        var dot = CalculateDotProduct(heroForward, youToVillain);
+
+        text += "\nDot " + dot;
+        if (dot >= 0)
+        {
+            text += "\nVillain is in front of hero";
+        }
+        else
+        {
+            text += "\nVillain is behind hero";
+        }
+        Text.text = text;
+    }
+
+    private float CalculateDotProduct(Vector3 v1, Vector3 v2)
+    {
+        return v1.x * v2.x + v1.z * v2.z; // ignore Y-axis
+    }
+}
---
+++ Assets/Scripts/VillainController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class VillainController : MonoBehaviour
+{
+    public GameObject Hero;
+    public GameObject Villain;
+    private CharacterController _controller;
+
+    private void Start()
+    {
+        _controller = Villain.GetComponent<CharacterController>();   
+    }
+
+    private void Update()
+    {
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+
+        var direction = new Vector3(hor, 0, ver);
+        _controller.Move(direction*.2f);
+
+        Villain.transform.LookAt(Hero.transform);
+    }
+}
